d999a6e13f935a7e29a62387d24b173179d09e08
Chris Agbolade <yemluck@gmail.com>
2022-01-29 17:19:22 -0600

    routes set up for details and add movie

---
+++ src/components/Details/Details.js
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux'
+import axios from 'axios';
+import { useHistory, Link } from 'react-router-dom';
+import './Details.css';
+
+function Details (){
+
+return (
+    <>
+    <h1> This is the details page </h1>
+    </>
+
+)
+}
+
+export default Details;
